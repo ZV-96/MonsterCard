5d8dd038510a74b65a0ac28535fb4c3399f1e146
ZV-96 <98915389+ZV-96@users.noreply.github.com>
2023-05-01 13:57:36 +1200

    update

---
+++ 01_Welcome_v1.py
@@ -0,0 +1,14 @@
+"""Version 1 of the welcome screen without
+easyGUI"""
+
+# Welcome statement
+print("Welcome to Monster Card Catalogue")
+
+# Ask user to enter a number
+choice = input("What would you like to do?\n"
+               "1) Add card\n"
+               "2) Delete card\n"
+               "3) Search card\n"
+               "4) Output catalogue\n"
+               "5) Exit\n"
+               "Enter number here: ")
